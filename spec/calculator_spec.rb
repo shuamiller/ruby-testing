e549ec5fc3e5719322bf233e146bd7bf51cd9883
Shua Miller <thejoshmill@gmail.com>
2021-01-11 16:46:39 -0700

    Add new test for add method.

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -6,5 +6,10 @@ describe Calculator do
       calculator = Calculator.new
       expect(calculator.add(5, 2)).to eql(7)
     end
+
+    it "returns the sum of more than two numbers" do
+      calculator = Calculator.new
+      expect(calculator.add(2, 5, 7)).to eql(14)
+    end
   end
 end
